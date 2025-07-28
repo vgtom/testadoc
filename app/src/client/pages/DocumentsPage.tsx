04a9c942b06a6f3e879aa01606fb6b3e8d88d12d
ashrafbinahmad <ashrafp216@gmail.com>
2025-07-28 18:07:51 +0530

    - added pdf editor with features: - add assets - not saved currently in db - draw sign in canvas - drag and drop the signs anywhere in the screen - protected editting and previewing to that user only - made preview look better - made documents listing page, new pdf adding progress bar

--- app/src/client/pages/DocumentsPage.tsx
+++ app/src/client/pages/DocumentsPage.tsx
@@ -1,13 +1,10 @@
 import React, { FC } from 'react'
-import ProtectedLayout from '../layouts/ProtectedLayout'
 import withHeader from '../HOC/withProtectedLayout'
-import FileUploadPage from '../../file-upload/FileUploadPage'
-import DocumentsContainer from '../../file-upload/DocumentsContainer'
+import DocumentsContainer from '../../document-upload/DocumentsContainer'
 
 const DocumentsPage: FC = () => {
   return (
     <div>
-        {/* <FileUploadPage /> */}
         <DocumentsContainer />
     </div>
   )
